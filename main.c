5ed5ca64f72b28cbfe09aea0efd2ac6d16bf2504
Andrew Gibbons <andrew.h.gibbons@protonmail.com>
2019-05-21 09:01:50 +0900

    Changing message read function.

--- main.c
+++ main.c
@@ -40,36 +40,64 @@ unsigned int getfilesize(FILE *fp) {
 	return filesize;
 };
 
+size_t readchunk(char *chunkbuffer, int chunksize, FILE *fp) {
+	size_t readsize;
 
+	readsize = fread(chunkbuffer, 1, chunksize, fp);
+
+	printf("%zu\n", readsize);
+
+	return readsize;
+}
 
 
 int main(int argc, char const *argv[])
 {
 
-	// Load file
-	FILE *fp;
+	
 
 	char filename[25] = "testlonger.txt";
 	char messagebuffer[BLOCKSIZE_BYTE];
 	char unsigned *messagebuffer2;
 	unsigned int filesize;
 
+
+	// Open file
+	FILE *fp;
 	fp = fopen(filename, "r");
 	filesize = getfilesize(fp); // length in bytes.
 	messagebuffer2 = (char *) malloc(filesize);
 	fread(messagebuffer2, 1, filesize, fp);
 	fseek(fp, 0L, SEEK_SET);
 
-	// Pad message
-	messagebuffer2 = padmessage(messagebuffer2, filesize);
-	if (messagebuffer2==NULL) {
-		return MEMORYERROR;
+	int chunksize;
+	unsigned char *chunkbuffer = (char *) malloc(BLOCKSIZE_BYTE);
+	
+	chunksize = 64;
+	fread(chunkbuffer, 1, 64, fp);
+	for (int i=0;i<chunksize;i++) {
+		printf("%c", chunkbuffer[i]);
 	}
+	printf("\n");
+
+	readchunk(chunkbuffer, 64, fp);
+	for (int i=0;i<chunksize;i++) {
+		printf("%c", chunkbuffer[i]);
+	}
+	printf("\n");
+
+	readchunk(chunkbuffer, 64, fp);
+	for (int i=0;i<chunksize;i++) {
+		printf("%c", chunkbuffer[i]);
+	}
+	printf("\n");
+
+
 
 	//Initial hash value
 	unsigned char H[64];
 	memcpy(H, H0, 64);
-	charprint(H, 64);
+	//charprint(H, 64);
 	
 
 	// Clean up
