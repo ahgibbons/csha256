e0190e2525774c3f037c6531c38aeaf862a47d44
Andrew Gibbons <andrew.h.gibbons@protonmail.com>
2019-05-26 16:31:53 +0900

    WORKING! SHA256 hash function working. Agrees with sha256sum for a number of random text files.

--- main.c
+++ main.c
@@ -46,14 +46,62 @@ unsigned int getfilesize(FILE *fp) {
 	return filesize;
 };
 
+
+void hashRound(uint32_t *wordbuffer, uint32_t *H) {
+
+
+	uint32_t a,b,c,d,e,f,g,h;
+	uint32_t T1,T2;
+	uint32_t W[64];
+	unsigned int t;
+	
+	a = H[0];
+	b = H[1];
+	c = H[2];
+	d = H[3];
+	e = H[4];
+	f = H[5];
+	g = H[6];
+	h = H[7];
+
+	// Make message schedule
+	memcpy(W, wordbuffer, BLOCKSIZE_BYTE);
+	for (t=16;t<64;t++) {
+		W[t] = sigma1(W[t-2]) + W[t-7] + sigma0(W[t-15]) + W[t-16];
+	}
+
+
+	for (t=0;t<64;t++) {
+		T1 = h + LSigma1(e) + Ch(e,f,g) + K[t] + W[t];
+		T2 = LSigma0(a) + Maj(a,b,c);
+		h = g;
+		g = f;
+		f = e;
+		e = d + T1;
+		d = c;
+		c = b;
+		b = a;
+		a = T1+T2;
+
+	}
+
+
+	H[0] += a;
+	H[1] += b;
+	H[2] += c;
+	H[3] += d;
+	H[4] += e;
+	H[5] += f;
+	H[6] += g;
+	H[7] += h;
+
+}
+
 uint32_t *hashMessage(FILE *fp) {
 	unsigned char bytebuffer[BLOCKSIZE_BYTE];
 	uint32_t wordbuffer[BLOCKNUM]; 
 
-	uint32_t a,b,c,d,e,f,g,h;
-	uint32_t T1,T2;
-	unsigned int messageIndex = 0;
-	uint32_t W[64];
+	
 
 
 	uint32_t *H;
@@ -61,26 +109,18 @@ uint32_t *hashMessage(FILE *fp) {
 	memcpy(H, H0, 32);
 
 
-	unsigned int t;
+	
 	unsigned int i;
 	unsigned int messagelength=0;
 	size_t readval;
 
-	printf("Hash0:\n");
-	for (i=0;i<8;i++) {
-		printf("%08x\n", H[i]);
-	}
 
-	while ((readval=fread(bytebuffer,1,BLOCKSIZE_BYTE,fp))>0) {
-		messageIndex++;
+	while ((readval=fread(bytebuffer,1,
+		BLOCKSIZE_BYTE,fp))==BLOCKSIZE_BYTE) {
 		messagelength += readval;
-		for (int i=0;i<readval;i++) {
-			printf("%c", bytebuffer[i]);
-		}
 
 
 		// Pad Message chunk if necessary
-		printf("\nMessage Index: %u\n", messageIndex);
 		if (readval < BLOCKSIZE_BYTE) {
 			padmessage(bytebuffer, readval, 
 						messagelength);
@@ -88,79 +128,16 @@ uint32_t *hashMessage(FILE *fp) {
 
 		// Convert to uint32 words
 		bytesToWords(wordbuffer, bytebuffer);
-		for (i=0;i<4;i++) {
-			printf("%08x ", (uint32_t) bytebuffer[i] << ((3-i)*8));
-		}
-		printf("\n");
-		printf("%08x\n", wordbuffer[0]);
 
-
-		printf("\nPadded Message:\n");
-		for (i=0;i<64;i++) {
-			printf("%d\t" BYTE_TO_BINARY_PATTERN "\n", 
-				i, BYTE_TO_BINARY(bytebuffer[i]));
-		}
-
-		printf("\n");
-		for (i=0;i<BLOCKNUM;i++) {
-			printf("%08x\n", wordbuffer[i]);
-		}
-		printf("\n");
-
-
-		a = H[0];
-		b = H[1];
-		c = H[2];
-		d = H[3];
-		e = H[4];
-		f = H[5];
-		g = H[6];
-		h = H[7];
-
-		// Make message schedule
-		memcpy(W, wordbuffer, BLOCKSIZE_BYTE);
-		for (t=16;t<64;t++) {
-			W[t] = sigma1(W[t-2]) + W[t-7] + sigma0(W[t-15]) + W[t-16];
-		}
-
-		printf("Message Schedule:\n");
-		for (int i=0; i<64; i++) {
-			printf("%d\t%08x\n", i, W[i]);
-		}
-
-		printf("Old a %08x\n", a);
-
-		for (t=0;t<64;t++) {
-			T1 = h + LSigma1(e) + Ch(e,f,g) + K[t] + W[t];
-			T2 = LSigma0(a) + Maj(a,b,c);
-			h = g;
-			g = f;
-			f = e;
-			e = d + T1;
-			d = c;
-			c = b;
-			b = a;
-			a = T1+T2;
-			printf("t: %u\t T1: %08x T2: %08x a: %08x b: %08x c: %08x d: %08x e: %08x f: %08x g: %08x h: %08x\n",
-					t, T1, T2, a,b,c,d,e,f,g,h);
-		}
-
-		printf("New a %08x\n", a);
-
-
-		H[0] += a;
-		H[1] += b;
-		H[2] += c;
-		H[3] += d;
-		H[4] += e;
-		H[5] += f;
-		H[6] += g;
-		H[7] += h;
+		hashRound(wordbuffer, H);
 
 
 	}
-	printf("Message length %d\n", messagelength);
-	printf("Message length (bits) %d\n", messagelength*8);
+
+	messagelength += readval;
+	padmessage(bytebuffer, readval, messagelength);
+	bytesToWords(wordbuffer, bytebuffer);
+	hashRound(wordbuffer, H);
 
 	return H;
 }
@@ -169,7 +146,6 @@ uint32_t *hashMessage(FILE *fp) {
 int main(int argc, char const *argv[])
 {
 	unsigned int filesize;
-	printf("%s\n", argv[1]);
 
 
 	// Open file
@@ -180,9 +156,9 @@ int main(int argc, char const *argv[])
 	digest = hashMessage(fp);
 
 
-	printf("%08x %08x %08x %08x %08x %08x %08x %08x\n", 
+	printf("%08x%08x%08x%08x%08x%08x%08x%08x  %s\n", 
 		digest[0], digest[1], digest[2], digest[3], digest[4],
-		digest[5], digest[6], digest[7]);
+		digest[5], digest[6], digest[7], argv[1]);
 
 
 	// Clean up
